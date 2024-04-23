a721f60d480f3b5e4ed15570d2b32eb2fca18e72
Timat-Piderma <mttpecc@gmail.com>
2024-04-23 11:52:23 +0200

    added padding to text

--- lib/item_card_home.dart
+++ lib/item_card_home.dart
@@ -44,6 +44,7 @@ class _ItemCardHomeState extends State<ItemCardHome> {
               Expanded(
                 flex: 4,
                 child: Container(
+                  padding: EdgeInsets.all(8.0),
                   color: Colors.blue,
                   child: Text(
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
