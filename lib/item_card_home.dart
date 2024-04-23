02e9bab155a822b5d1a127facd83c685f3586e5a
Timat-Piderma <mttpecc@gmail.com>
2024-04-23 11:28:40 +0200

    added text structure

--- lib/item_card_home.dart
+++ lib/item_card_home.dart
@@ -35,7 +35,42 @@ class _ItemCardHomeState extends State<ItemCardHome> {
         )),
         Expanded(
             child: Container(
-          color: Colors.blue,
+          child: Column(
+            children: [
+              Text(
+                "Articolo del Giorno",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              Expanded(
+                flex: 4,
+                child: Container(
+                  color: Colors.blue,
+                  child: Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 5,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Center(child: Text("9.99 €",
+                    style: Theme.of(context).textTheme.bodySmall,)),
+                  ),
+                  Expanded(
+                      child: FilledButton(
+                    onPressed: () => {},
+                    child: Text(
+                      "Shop Now",
+                      style: Theme.of(context).textTheme.labelLarge,
+                    ),
+                  ))
+                ],
+              )
+            ],
+          ),
         )),
       ],
     );
