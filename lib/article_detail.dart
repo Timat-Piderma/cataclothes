615d63ec4f824b59bb03028b0518b4faef2d1cd0
Timat-Piderma <mttpecc@gmail.com>
2024-04-26 07:34:34 +0200

    Modified article detail screen layout

--- lib/article_detail.dart
+++ lib/article_detail.dart
@@ -34,6 +34,13 @@ class _ArticleDetailState extends State<ArticleDetail>
     super.dispose();
   }
 
+  double computeWidth() {
+    double width = MediaQuery.of(context).size.width;
+    var padding = MediaQuery.of(context).viewPadding;
+    double safeWidth = width - padding.left - padding.right;
+    return safeWidth;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,11 +55,10 @@ class _ArticleDetailState extends State<ArticleDetail>
       body: ListView(
         children: <Widget>[
           Container(
-            width: MediaQuery.of(context).size.width,
-            height: 250,
+            width: computeWidth() / 2,
+            height: computeWidth() /2,
             decoration: BoxDecoration(
               image: DecorationImage(
-                fit: BoxFit.fill,
                 image: AssetImage(widget.article.image),
               ),
             ),
@@ -62,183 +68,120 @@ class _ArticleDetailState extends State<ArticleDetail>
                   bottom: 8,
                   right: 8,
                   child: IconButton(
-                      icon: Icon(_isFavourited ? Icons.favorite : Icons.favorite_border),
+                      icon: Icon(_isFavourited
+                          ? Icons.favorite
+                          : Icons.favorite_border),
                       iconSize: 28,
                       color: Colors.orange,
                       onPressed: () {
                         setState(() {
                           _isFavourited = !_isFavourited;
-                          final dataManager = Provider.of<DataManager>(context, listen: false);
-                          dataManager.updateFavouriteArticleValue(widget.article, _isFavourited);
-                          dataManager.updateFavouriteArticlesList(widget.article, _isFavourited);
+                          final dataManager =
+                              Provider.of<DataManager>(context, listen: false);
+                          dataManager.updateFavouriteArticleValue(
+                              widget.article, _isFavourited);
+                          dataManager.updateFavouriteArticlesList(
+                              widget.article, _isFavourited);
                         });
                       }),
                 )
               ],
             ),
           ),
-          TabBar(
-            controller: _tabController,
-            tabs: const [
-              Tab(
-                text: "Description",
+          Column(
+            children: [
+              SizedBox(
+                height: 8,
               ),
-              Tab(
-                text: "Ingredients",
-              ),
-              Tab(
-                text: "Method",
+              ListView(
+                primary: false,
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Nome",
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      Text(
+                        widget.article.name,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Costo",
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      Text(
+                        widget.article.cost,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Difficulty",
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      Text(
+                        " testo ",
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Cost",
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      Text(
+                        "${widget.article.cost}",
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Preparation time",
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      Text(
+                        "ALTRO TESTO",
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ],
-            onTap: (int index) {
-              setState(() {
-                _selectedIndex = index;
-                _tabController.animateTo(index);
-              });
-            },
-          ),
-          IndexedStack(
-            index: _selectedIndex,
-            children: <Widget>[
-              Visibility(
-                child: buildDescriptionPanel(),
-                maintainState: true,
-                visible: _selectedIndex == 0,
-              ),
-              Visibility(
-                child: buildIngredientsPanel(),
-                maintainState: true,
-                visible: _selectedIndex == 1,
-              ),
-              Visibility(
-                child: buildMethodPanel(),
-                maintainState: true,
-                visible: _selectedIndex == 2,
-              ),
-            ],
-          ),
+          )
         ],
       ),
-    );
-  }
-
-  Widget buildDescriptionPanel() {
-    return Column(
-      children: [
-        SizedBox(
-          height: 8,
-        ),
-        ListView(
-          primary: false,
-          physics: const NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Course",
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                Text(
-                  " TESTOOOOOO ",
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Cuisine",
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                Text(
-                  " TESTOOO ",
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Difficulty",
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                Text(
-                  " testo ",
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Cost",
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                Text(
-                  "${widget.article.cost}",
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Preparation time",
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                Text(
-                  "ALTRO TESTO",
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              ],
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-
-  Widget buildIngredientsPanel() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
-          child: Text(
-            "For 999 serving(s)",
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget buildMethodPanel() {
-    return Column(
-      children: [
-        SizedBox(
-          height: 8,
-        ),
-      ],
     );
   }
 }
