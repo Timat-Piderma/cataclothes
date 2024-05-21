2b29fb308b383592b4d807b2afd078866e951bc0
Timat-Piderma <mttpecc@gmail.com>
2024-05-21 11:02:21 +0200

    added images to sample data

--- lib/sample_data.dart
+++ lib/sample_data.dart
@@ -5,307 +5,174 @@ import 'article_color.dart';
 import 'outfit.dart';
 
 class SampleData {
-  static Article article1 = Article(
+  static Article cappello1 = Article(
     name: 'Berretto',
     image: 'assets/articles/berretto.jpg',
     cost: "10.00 €",
     isFavourite: false,
-    color: red,
+    color: black,
     category: cat1,
   );
 
-  static Article article2 = Article(
-    name: 'Berretto2',
-    image: 'assets/articles/berretto.jpg',
+  static Article cappello2 = Article(
+    name: 'Cappello2',
+    image: 'assets/articles/cappello2.jpg',
+    cost: "10.00 €",
+    isFavourite: false,
+    color: black,
+    category: cat1,
+  );
+
+  static Article cappello3 = Article(
+    name: 'Cappello3',
+    image: 'assets/articles/cappello3.jpg',
+    cost: "10.00 €",
+    isFavourite: false,
+    color: gray,
+    category: cat1,
+  );
+
+  static Article gonna1 = Article(
+    name: 'Gonna1',
+    image: 'assets/articles/gonna1.jpg',
+    cost: "10.00 €",
+    isFavourite: false,
+    color: teal,
+    category: cat1,
+  );
+
+  static Article gonna2 = Article(
+    name: 'Gonna2',
+    image: 'assets/articles/gonna2.jpg',
     cost: "10.00 €",
     isFavourite: false,
     color: red,
     category: cat1,
   );
 
-  static Article article3 = Article(
-    name: 'Berretto3',
-    image: 'assets/articles/berretto.jpg',
+  static Article maglietta4 = Article(
+    name: 'Maglietta4',
+    image: 'assets/articles/hawaii.jpg',
+    cost: "10.00 €",
+    isFavourite: false,
+    color: blue,
+    category: cat1,
+  );
+
+  static Article jeans1 = Article(
+    name: 'Jeans1',
+    image: 'assets/articles/jeans1.jpg',
+    cost: "10.00 €",
+    isFavourite: false,
+    color: gray,
+    category: cat1,
+  );
+
+  static Article jeans2 = Article(
+    name: 'Jeans2',
+    image: 'assets/articles/jeans2.jpg',
+    cost: "10.00 €",
+    isFavourite: false,
+    color: blue,
+    category: cat1,
+  );
+
+  static Article jeans3 = Article(
+    name: 'Jeans3',
+    image: 'assets/articles/jeans3.jpg',
+    cost: "10.00 €",
+    isFavourite: false,
+    color: blue,
+    category: cat1,
+  );
+
+  static Article maglietta1 = Article(
+    name: 'Maglietta1',
+    image: 'assets/articles/maglietta1.jpg',
     cost: "10.00 €",
     isFavourite: false,
     color: red,
     category: cat1,
   );
 
-  static Article article4 = Article(
-    name: 'Berretto4',
-    image: 'assets/articles/berretto.jpg',
+  static Article maglietta2 = Article(
+    name: 'Maglietta2',
+    image: 'assets/articles/maglietta2.jpg',
+    cost: "10.00 €",
+    isFavourite: false,
+    color: black,
+    category: cat1,
+  );
+
+  static Article maglietta3 = Article(
+    name: 'Maglietta3',
+    image: 'assets/articles/maglietta3.jpg',
+    cost: "10.00 €",
+    isFavourite: false,
+    color: black,
+    category: cat1,
+  );
+
+  static Article scarpe1 = Article(
+    name: 'Scarpe1',
+    image: 'assets/articles/scarpe1.jpg',
+    cost: "10.00 €",
+    isFavourite: false,
+    color: gray,
+    category: cat1,
+  );
+
+  static Article scarpe2 = Article(
+    name: 'Scarpe2',
+    image: 'assets/articles/scarpe2.jpg',
     cost: "10.00 €",
     isFavourite: false,
     color: red,
     category: cat1,
   );
 
-  static Article article5 = Article(
-    name: 'Berretto5',
-    image: 'assets/articles/berretto.jpg',
+  static Article scarpe3 = Article(
+    name: 'Scarpe3',
+    image: 'assets/articles/scarpe3.jpg',
     cost: "10.00 €",
     isFavourite: false,
-    color: red,
+    color: blue,
     category: cat1,
   );
 
-  static Article article11 = Article(
-    name: 'Berretto',
-    image: 'assets/articles/berretto.jpg',
+  static Article shorts1 = Article(
+    name: 'Shorts1',
+    image: 'assets/articles/shorts1.jpg',
     cost: "10.00 €",
     isFavourite: false,
-    color: red,
+    color: blue,
     category: cat1,
   );
 
-  static Article article12 = Article(
-    name: 'Berretto2',
-    image: 'assets/articles/berretto.jpg',
+  static Article shorts2 = Article(
+    name: 'Shorts2',
+    image: 'assets/articles/shorts2.jpg',
     cost: "10.00 €",
     isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article13 = Article(
-    name: 'Berretto3',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article14 = Article(
-    name: 'Berretto4',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article15 = Article(
-    name: 'Berretto5',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article21 = Article(
-    name: 'Berretto',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article22 = Article(
-    name: 'Berretto2',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article23 = Article(
-    name: 'Berretto3',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article24 = Article(
-    name: 'Berretto4',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article25 = Article(
-    name: 'Berretto5',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article31 = Article(
-    name: 'Berretto',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article32 = Article(
-    name: 'Berretto2',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article33 = Article(
-    name: 'Berretto3',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article34 = Article(
-    name: 'Berretto4',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article35 = Article(
-    name: 'Berretto5',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article41 = Article(
-    name: 'Berretto',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article42 = Article(
-    name: 'Berretto2',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article43 = Article(
-    name: 'Berretto3',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article44 = Article(
-    name: 'Berretto4',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article45 = Article(
-    name: 'Berretto5',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article51 = Article(
-    name: 'Berretto',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article52 = Article(
-    name: 'Berretto2',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article53 = Article(
-    name: 'Berretto3',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article54 = Article(
-    name: 'Berretto4',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
-    category: cat1,
-  );
-
-  static Article article55 = Article(
-    name: 'Berretto5',
-    image: 'assets/articles/berretto.jpg',
-    cost: "10.00 €",
-    isFavourite: false,
-    color: red,
+    color: blue,
     category: cat1,
   );
 
   static List<Article> allArticles = [
-    /*article1,
-    article2,
-    article3,
-    article4,
-    article5,
-    article11,
-    article12,
-    article13,
-    article14,
-    article15,
-    article21,
-    article22,
-    article23,
-    article24,
-    article25,
-    article31,
-    article32,
-    article33,
-    article34,
-    article35,
-    article41,
-    article42,
-    article43,
-    article44,
-    article45,
-    article51,
-    article52,
-    article53,
-    article54,
-    article55,*/
+    cappello1,
+    cappello2,
+    cappello3,
+    gonna1,
+    gonna2,
+    maglietta1,
+    maglietta2,
+    maglietta3,
+    maglietta4,
+    scarpe1,
+    scarpe2,
+    scarpe3,
+    shorts1,
+    shorts2
   ];
 
   static List<Article> favouriteArticles = [];
@@ -339,12 +206,12 @@ class SampleData {
     isFavourite: false,
   );
 
-  static List<Outfit> allOutfits = [/*outfit1, outfit2, outfit3, outfit4*/];
+  static List<Outfit> allOutfits = [outfit1, outfit2, outfit3, outfit4];
 
   static List<Outfit> favouriteOutfits = [];
 
   static List<Article> featuredItems = [
-    article1,
+    maglietta3,
   ];
 
   static Category cat1 = Category(name: 'cat1', id: 1);
@@ -358,12 +225,18 @@ class SampleData {
   ];
 
   static ArticleColor red = ArticleColor(name: 'red', id: 1);
-  static ArticleColor blue = ArticleColor(name: 'blue', id: 1);
-  static ArticleColor yellow = ArticleColor(name: 'yellow', id: 1);
+  static ArticleColor blue = ArticleColor(name: 'blue', id: 2);
+  static ArticleColor yellow = ArticleColor(name: 'yellow', id: 3);
+  static ArticleColor black = ArticleColor(name: 'black', id: 4);
+  static ArticleColor gray = ArticleColor(name: 'gray', id: 5);
+  static ArticleColor teal = ArticleColor(name: 'teal', id: 6);
 
   static List<ArticleColor> allColors = [
     red,
     blue,
     yellow,
+    black,
+    gray,
+    teal,
   ];
 }
